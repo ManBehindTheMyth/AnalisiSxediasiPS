356cfa37b73ee1bc954f40148e6bf3e8ad1a1368
apoger <grumoxhd@gmail.com>
2021-03-03 12:59:57 +0200

    gio

--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
     console.log("we are ok!");
 })
-
-$(".custom-file-input").on("change", function() {
-  var fileName = $(this).val().split("\\").pop();
-  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-});
